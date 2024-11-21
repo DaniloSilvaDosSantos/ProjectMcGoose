296d99a8e7d32dc89f4e9a762542515e7b8f9722
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2024-11-21 16:04:50 -0300

    TNT Explosion bug fix

--- Assets/Scripts/TNTController.cs
+++ Assets/Scripts/TNTController.cs
@@ -6,22 +6,35 @@ using UnityEngine;
 public class TNTController : MonoBehaviour
 {
     private Rigidbody2D rb;
-
-    public GameObject explosion;
+    public GameObject explosionPrefab;
+    private GameObject explosion;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+        explosion.transform.SetParent(transform);
+        explosion.SetActive(false);
     }
 
     void OnDestroy()
     {
-        Instantiate(explosion, transform.position, transform.rotation);
+        explosion.SetActive(true);
+        explosion.transform.SetParent(null);
+
+        Animator animator;
+        animator = explosion.GetComponent<Animator>();
+        animator.enabled = true;
+
+        CircleCollider2D col;
+        col = explosion.GetComponent<CircleCollider2D>();
+        col.enabled = true;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(rb.velocity.x >= 1 || rb.velocity.y >= 1)
+        if(rb.velocity.x >= 2 || rb.velocity.y >= 2)
         {
             Destroy(gameObject);
         }
