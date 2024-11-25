7099423eab7b3a48e3ba5186e62d6251ca9c90d9
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2024-11-25 20:34:50 -0300

    TNT Changes and implementation of animations

--- Assets/Scripts/TNTController.cs
+++ Assets/Scripts/TNTController.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class TNTController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Animator animator;
     public GameObject explosionPrefab;
     private GameObject explosion;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
         explosion.transform.SetParent(transform);
@@ -23,9 +26,17 @@ public class TNTController : MonoBehaviour
         ActivatingExplosion();
     }
 
+    void Update()
+    {
+        animator.SetFloat("VelocityY", rb.velocity.y);
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(rb.velocity.x >= 2 || rb.velocity.y >= 2)
+
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy")) Destroy(gameObject);
+
+        if(math.abs(rb.velocity.x) >= 2 || rb.velocity.y >= 2)
         {
             Destroy(gameObject);
         }
